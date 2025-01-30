306a4fba979f682a865769a6d373eb8756a8d3e0
gastonrobledo23 <gastonrobledo44@gmail.com>
2025-01-30 03:25:52 +0100

    arrreglo de enlaces

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
